c72d2df632de561788c6f3f89fb6dbcb07719edd
evg2108 <evg2108@yandex.ru>
2015-12-31 16:19:00 +0700

    fix

--- lib/strong_permitter/manager.rb
+++ lib/strong_permitter/manager.rb
@@ -4,7 +4,7 @@ module StrongPermitter
       @permission_class ||= "#{self.class.name.sub('Controller', '')}Permission".camelcase.safe_constantize
       return nil unless @permission_class
 
-      action_hash = @permission_class.actions[action_name]
+      action_hash = @permission_class.actions[action_name.to_sym]
 
       resource_name = action_hash[:resource] || @permission_class.resource_name || controller_name.singularize
       allowed_params = action_hash[:permitted_params]
